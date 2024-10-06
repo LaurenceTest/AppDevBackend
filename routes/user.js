495a9997f768b1cda58c094c8089b2757db608a3
LaurenceTest <reylaurencecapoy@gmail.com>
2024-10-06 20:53:44 +0800

    Added wildcard route catch

--- routes/user.js
+++ routes/user.js
@@ -8,5 +8,7 @@ const UserController = require("../controllers/userController.js")
 router.post("/register",validateInput,UserController.register)
 router.post("/login",validateInput,UserController.login)
 router.get("/profile",authenticateToken,UserController.getProfile)
+//Catches any other routes
+router.all('/',(req,res)=>res.sendStatus(404))
 
 module.exports = router;
