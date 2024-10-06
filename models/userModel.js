96f60c63ab49adc2c96bc830b920ba1391ede6b0
LaurenceTest <reylaurencecapoy@gmail.com>
2024-10-06 14:35:26 +0800

    Removed id redundancy

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,20 @@
 const fs = require('node:fs')
 const path = require('node:path')
-const users = new Map()
+const users = []
 let increment = 0
 
 const UserModel = {
     create(user){
-        users.set(increment++,user)
-    },
-    get(id){
-        return users.get(id)
+        users.push({id:increment++,...user})
     },
     findUserByEmail(email){
-        for(const user of users.values()){
+        for(const user of users){
             if(user.email === email) return user
         }
         return null
     },
     findUserByName(username){
-        for(const user of users.values()){
+        for(const user of users){
             if(user.username === username) return user
         }
         return null
