d8289937e93a58e1aea9adba0ba007bce3973eff
LaurenceTest <reylaurencecapoy@gmail.com>
2024-10-06 14:06:30 +0800

    Adjusted error checking

--- controllers/userController.cjs
+++ controllers/userController.cjs
@@ -5,22 +5,25 @@ const jwt = require('jsonwebtoken')
 const UserController = {
     login: (req,res)=>{
         try {
-            const {email,username} = req.body
-            if(UserModel.findUser(email) !== null){
+            const {email,username,password} = req.body
+            const user = UserModel.findUserByEmail(email) ?? UserModel.findUserByName(username)
+            console.log(user)
+            if(user !== null && user?.password === password){
                 const token = jwt.sign({
-                    email,
-                    username
+                    email:user.email,
+                    username:user.username
                 },process.env.ACCESS_TOKEN)
                 res.status(200).send({user:token})
-            }else res.sendStatus(403)
+            }
+            else res.sendStatus(403)
         } catch (error) {
             res.sendStatus(500)
         }
     },
     register: (req,res)=>{
         try {
-            const {email} = req.body
-            if(UserModel.findUser(email) === null){
+            const {email,username} = req.body
+            if(UserModel.findUserByEmail(email) === null && UserModel.findUserByName(username) === null){
                 UserModel.create(req.body)
                 res.sendStatus(201)
             }else res.status(403).send({msg:"User already exists"})
@@ -30,7 +33,7 @@ const UserController = {
     },
     getProfile: (req,res)=>{
         try {
-            const user = UserModel.findUser(req.user.email)
+            const user = UserModel.findUserByEmail(req.user.email)
             if(user) res.status(200).send(user)
             else res.sendStatus(404)
         } catch (error) {
